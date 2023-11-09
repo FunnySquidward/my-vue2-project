a3d0e62bf3644e30e8c281def25f0696e8a25b56
ivanyb <ivanyb@qq.com>
2023-11-09 09:51:23 +0800

    完成新闻列表的item静态结构改造

--- src/News/Index.vue
+++ src/News/Index.vue
@@ -8,6 +8,18 @@
       title="新闻列表" />
 
    <!-- 2. 新闻item -->
+   <van-card
+         tag="热门"
+         price="2023-11-9 15:00:01"
+         currency=""
+         desc="描述信息"  
+         title="商品标题"
+         thumb="http://127.0.0.1:8888//upload/201504/16/201504161229442885.jpg"         
+         >
+         <div slot="num">
+           <span>点击:100</span>
+         </div>
+         </van-card>
    </div>
 </template>
 
